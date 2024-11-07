696fabffe79b2ba1d3a9c829a306a4cf9716f0ef
Vinicius <beneditoloura@gmail.com>
2024-11-06 21:23:09 -0300

    feat: create utils functions and tests

---
+++ utils/utils.ts
@@ -0,0 +1,28 @@
+import { theme } from "../constants";
+import ButtonProps from "../types";
+
+const getBgVariantStyle = (variant: ButtonProps["bgVariant"]) => {
+  const variants = {
+    primary: `bg-[${theme.colors.primary[500]}]`,
+    secondary: "bg-gray-500",
+    danger: "bg-red-500",
+    success: "bg-green-500",
+    outline: "bg-transparent border-neutral-300 border-[0.5px]",
+  };
+
+  return variants[variant!];
+};
+
+const getTextVariantStyle = (variant: ButtonProps["textVariant"]) => {
+  const variants = {
+    primary: "text-black",
+    secondary: "text-gray-100",
+    danger: "text-red-100",
+    success: "text-green-100",
+    default: `bg-[${theme.colors.primary[500]}]`,
+  };
+
+  return variants[variant!];
+};
+
+export { getBgVariantStyle, getTextVariantStyle };
