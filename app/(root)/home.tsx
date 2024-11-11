ea2c37edbff5088a59efd3e692099c5e5dbf2269
Vinicius <beneditoloura@gmail.com>
2024-11-11 06:59:47 -0300

    feat: add first location code

--- app/(root)/home.tsx
+++ app/(root)/home.tsx
@@ -1,12 +1,29 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Text } from "react-native";
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+  LocationObject,
+} from "expo-location";
+import { useEffect, useState } from "react";
 
 const Home = () => {
-  return (
-    <SafeAreaView>
-      <Text>Home</Text>
-    </SafeAreaView>
-  );
+  const [location, setLocation] = useState<LocationObject | null>(null);
+
+  const requestLocationPermissions = async () => {
+    const { granted } = await requestForegroundPermissionsAsync();
+
+    if (granted) {
+      const currentPosition = await getCurrentPositionAsync();
+      setLocation(currentPosition);
+      console.log(currentPosition);
+    }
+  };
+
+  useEffect(() => {
+    requestLocationPermissions();
+  }, []);
+
+  return <SafeAreaView></SafeAreaView>;
 };
 
 export default Home;
