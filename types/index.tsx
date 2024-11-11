b425d483058122e29abda871b0f8d9767fc3a309
Vinicius <beneditoloura@gmail.com>
2024-11-11 06:58:03 -0300

    feat: create onboarding component interface

--- types/index.tsx
+++ types/index.tsx
@@ -1,9 +1,17 @@
-import { TouchableOpacityProps } from "react-native";
+import { ImageProps, TouchableOpacityProps } from "react-native";
 
-export default interface ButtonProps extends TouchableOpacityProps {
+export interface ButtonProps extends TouchableOpacityProps {
   onPress: () => void;
   title: string;
   bgVariant?: "primary" | "secondary" | "danger" | "success" | "outline";
   textVariant?: "primary" | "default" | "secondary" | "danger" | "success";
   className?: string;
+}
+
+export interface OnboardingProps {
+  onboarding: {
+    description: string;
+    image: ImageProps;
+    title: string;
+  }[];
 }
